a997c460e6fa003b3f75b2d7e0065591eb94ae4b
Dean <dean@deangaudet.com>
2018-07-24 20:51:36 -0600

    - Adding current work in the learnnode.js course

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+// Do work here
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send('test ' + reverse);
+});
+
+module.exports = router;
