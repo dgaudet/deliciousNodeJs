4fc10de17ea64f353e8512f5b36d29c20724540b
Dean <dean@deangaudet.com>
2018-07-27 19:59:54 -0600

    - storeController.js - adding routes to upload and resize a photo, as well as display an existing store - Store.js - adding the store photo path - index.js - adding routes to add/update a store with a photo, as well as a route to view an existing store - _storeCard.pug - fix image location - _storeForm.pug - adding ablity to upload a photo - store.pug - adding view to show an existing store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({message: 'That filetype isn\'t allowed'}, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -11,6 +25,22 @@ exports.addStore = (req, res) => {
     title: 'Add Store'
   });
 };
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async(req, res, next) => {
+  if (!req.file){
+    next(); // skips to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // resize the Photo
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+}
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
@@ -42,4 +72,13 @@ exports.updateStore = async (req, res) => {
 
   req.flash('success', `Successfuly Updated ${store.name}. <a href="/stores/${store.slug}">View Store</a>`);
   res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug});
+  if (!store){
+    return next();
+  }
+  //confirm they are the owner of the store
+  res.render('store', { store, title: `${store.name}`});
 };
